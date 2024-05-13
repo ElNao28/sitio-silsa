6b5a0bd3cd5c12b5f6c4172ca90cc9e6de36843b
Luis231968 <202000770@uthh.edu.mx>
2024-05-13 13:02:06 -0600

    dashboard terminado

--- src/app/admin/pages/administrar-citas/administrar-citas.component.ts
+++ src/app/admin/pages/administrar-citas/administrar-citas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SilsaService } from '../../../user/services/silsa.service';
+import { GetCitas, Horario } from '../../../user/interfaces/GetCitas.interface';
 
 @Component({
   selector: 'app-administrar-citas',
@@ -6,11 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './administrar-citas.component.css'
 })
 export class AdministrarCitasComponent implements OnInit{
-  fecha = new Date().toLocaleDateString();
+
+  constructor(private silsaService:SilsaService){}
+
+  public citas:GetCitas[] = [];
+  public fecha = new Date().toLocaleDateString();
+  public dayActually:number = parseInt(this.fecha.split('/')[0]);
+  public days: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  public horarios:Horario[] = [];
+  public daySelected:number = 0;
+
   ngOnInit(): void {
-
-
+    this.silsaService.getHorarios().subscribe(data =>{
+      this.citas = data;
+    });
+    this.daySelected = this.dayActually;
   }
 
+  isActive(day:number){
+    for(let i = 0; i< this.citas.length; i++){
+      if(this.citas[i].dia === day){
+        return true;
+      }
+    }
+    return false;
+  }
 
+  showHorarios(day:number){
+    this.daySelected = day;
+    for(let i = 0; i< this.citas.length; i++){
+      if(this.citas[i].horarios.length <= 0){
+        this.horarios = [];
+        return;
+      }
+      if(this.citas[i].dia === day){
+        this.horarios = this.citas[i].horarios;
+      }
+    }
+  }
 }
--- src/app/user/pages/inicio/inicio.component.ts
+++ src/app/user/pages/inicio/inicio.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
+  public isLoading: boolean = true;
   imagenes:string[] = [
     'https://img.freepik.com/foto-gratis/papel-pintado-gatito-lindo-fantasia_1409-6188.jpg',
     'https://img.freepik.com/foto-gratis/colores-arremolinados-interactuan-danza-fluida-sobre-lienzo-que-muestra-tonos-vibrantes-patrones-dinamicos-que-capturan-caos-belleza-arte-abstracto_157027-2892.jpg',
     'https://i.blogs.es/0ca9a6/aa/1366_2000.jpeg'
   ];
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1700);
+  }
+
 }
